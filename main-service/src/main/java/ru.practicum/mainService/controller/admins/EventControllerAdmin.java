384990c54b4c18cd9b3a0e9be699d23eaac46a27
Pankov <pankovrec@yandex.ru>
2023-04-10 20:20:37 +0500

    2st

--- main-service/src/main/java/ru.practicum/mainService/controller/admins/EventControllerAdmin.java
+++ main-service/src/main/java/ru.practicum/mainService/controller/admins/EventControllerAdmin.java
@@ -2,7 +2,6 @@ package ru.practicum.mainService.controller.admins;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.mainService.dto.event.EventFullDto;
 import ru.practicum.mainService.dto.event.UpdateEventAdminRequest;
