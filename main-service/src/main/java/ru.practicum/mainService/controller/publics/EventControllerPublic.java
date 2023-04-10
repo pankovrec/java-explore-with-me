d619941576113547edb9328cbad11b5da27e9cd6
Pankov <pankovrec@yandex.ru>
2023-04-10 23:24:46 +0500

    2st

--- main-service/src/main/java/ru.practicum/mainService/controller/publics/EventControllerPublic.java
+++ main-service/src/main/java/ru.practicum/mainService/controller/publics/EventControllerPublic.java
@@ -2,16 +2,12 @@ package ru.practicum.mainService.controller.publics;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.StatsClient;
 import ru.practicum.mainService.dto.event.EventFullDto;
 import ru.practicum.mainService.service.publics.EventServicePublic;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 /**
@@ -21,7 +17,6 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequestMapping(path = "/events")
-@Validated
 
 public class EventControllerPublic {
 
@@ -42,15 +37,15 @@ public class EventControllerPublic {
                                         @RequestParam(name = "rangeEnd", required = false) String rangeEnd,
                                         @RequestParam(name = "onlyAvailable", required = false) Boolean onlyAvailable,
                                         @RequestParam(name = "sort", required = false) String sort,
-                                        @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-                                        @Positive @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                        @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                        @RequestParam(name = "size", defaultValue = "10") Integer size,
                                         HttpServletRequest request) {
         log.info("Get list events from={}, size={}", from, size);
         return eventService.getEvents(text, categories, paid, rangeStart, rangeEnd, onlyAvailable, sort, from, size, request);
     }
 
     @GetMapping(path = "/{id}")
-    public EventFullDto getEventById(@NotNull @PathVariable(name = "id") Long id, HttpServletRequest request) {
+    public EventFullDto getEventById(@PathVariable(name = "id") Long id, HttpServletRequest request) {
         log.info("Get full event by id={}", id);
         return eventService.getEventById(id, request);
     }
