3adba297e4b78b9a1581bee44ec178d28320cd7d
Pankov <pankovrec@yandex.ru>
2023-04-20 15:08:30 +0500

    2st

--- main-service/src/main/java/ru.practicum/mainService/dto/event/UpdateEventUserRequest.java
+++ main-service/src/main/java/ru.practicum/mainService/dto/event/UpdateEventUserRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.practicum.mainService.model.Location;
 import ru.practicum.mainService.model.StateAction;
 
 /**
@@ -15,6 +16,25 @@ import ru.practicum.mainService.model.StateAction;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateEventUserRequest {
+    private Long id;
+
+    private String annotation;
+
+    private Long category;
+
+    private String description;
+
     private String eventDate;
+
+    private Location location;
+
+    private Boolean paid;
+
+    private Integer participantLimit;
+
+    private Boolean requestModeration;
+
     private StateAction stateAction;
+
+    private String title;
 }
